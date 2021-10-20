569c93ca92d660992ad1515b2ebfdc5158abb834
石佳琪 <rt006814@rotai.com>
2021-10-20 12:24:14 +0800

    first commit

---
+++ src/main/java/controller/DemoController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：sjq
+ * @date ：Created in 2021/10/20 下午12:23
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class DemoController {
+
+}
